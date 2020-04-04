f6de173e9543ef2cb590c39bb6bc2a321bc8797a
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-04-04 20:49:44 +0200

    Connecting to mongoose, and taking middleware into use is move to app.js Index.js is now only responsible for creating connection with the server. This was the last part of the solution for part 4.2 blog-post part 2, where we refactor the app into separate modules.

---
+++ part4/blog-list/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+
+mongoose
+  .connect(config.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("connected to db");
+  })
+  .catch((err) => {
+    logger.error("Error connecting to db", err.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,35 +1,11 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const logger = require("./utils/logger");
+const app = require("./app");
+const http = require("http");
 const config = require("./utils/config");
-const blogsRouter = require("./controllers/blogs");
-const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
 
-mongoose
-  .connect(config.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    logger.info("connected to db");
-  })
-  .catch((err) => {
-    logger.error("Error connecting to db", err.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-app.use("/api/blogs", blogsRouter);
-
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+const server = http.createServer(app);
 
 const PORT = config.PORT;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });

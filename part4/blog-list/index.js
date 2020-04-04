9ee75c0f5139130620e22bf714bfa4d57098e7ca
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-04-04 20:27:19 +0200

    Extract route handlers to separate module (controllers/posts). Part of solution for exercise 4.2 blog-list part2.

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-const Blog = require("./models/blogs");
+const blogsRouter = require("./controllers/blogs");
 
 mongoose
   .connect(config.MONGO_URI, {
@@ -22,19 +22,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 const PORT = config.PORT;
 app.listen(PORT, () => {

7a608c1748c627d88f8404419226c0833a6a750f
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2020-04-04 20:20:53 +0200

    Extract mongoose model for blog into separate module (models/blogs). Part of solution for exercise 4.2 blog-list part2.

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -5,15 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blogs");
 
 mongoose
   .connect(config.MONGO_URI, {
---
+++ part4/blog-list/models/blogs.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
